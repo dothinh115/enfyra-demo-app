d2eaa313c7bb0114e4aec1e586c8f06c8b041a63
dothinh115 <dothinh115@gmail.com>
2025-12-19 10:45:26 +0700

    fix: update handler path in nuxt.config.ts for server middleware

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
     // },
     {
       route: "**",
-      handler: "server/middleware/server-id.ts",
+      handler: "~/server/middleware/server-id.ts",
       middleware: true,
     },
     {
